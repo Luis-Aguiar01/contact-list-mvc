62170f6552a37bc379edf03838d8a621ac21ee8d
Luis Henrique <luishenrique.aguiar@outlook.com>
2024-11-29 11:13:16 -0300

    Implementação do JDBC

---
+++ src/main/java/br/edu/ifsp/dsw1/model/dao/connection/ContactsDatabaseConnection.java
@@ -0,0 +1,25 @@
+package br.edu.ifsp.dsw1.model.dao.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ContactsDatabaseConnection {
+	private static final String URL = "jdbc:mysql://localhost:3307/contacts_db";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+	
+	private ContactsDatabaseConnection() { }
+	
+	static {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static Connection getConnection() throws SQLException {
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+}
